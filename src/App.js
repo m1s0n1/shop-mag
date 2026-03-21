0e625322e46d2274f8cc342e5faf7d97ae06ed37
m1s0n1 <mashatrunina10112005@gmail.com>
2026-03-21 16:40:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
+
+class App extends React.Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      orders: [],
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair-grey.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+          category: 'chairs',
+          price: '49.99'
+        },
+          {
+          id: 2,
+          title: 'Стол',
+          img: 'table.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+          category: 'tables',
+          price: '149.99'
+        },
+          {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+          category: 'sofa',
+          price: '549.99'
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'lamp.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+          category: 'light',
+          price: '25.00'
+        },
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair-white.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+          category: 'chairs',
+          price: '49.99'
+        }
+        
+      ]
+    }
+    this.addToOrder = this.addToOrder.bind(this)
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header orders={this.state.orders} />
+      <Items items={this.state.items} onAdd = {this.addToOrder} />
+      <Footer />
     </div>
   );
+  }
+
+  addToOrder(item) {
+    this.setState({orders: [...this.state.orders, item]})
+
+  }
 }
 
 export default App;
