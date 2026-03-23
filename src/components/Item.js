c9fc854459b693dca0157b04d028211a7f61a981
m1s0n1 <mashatrunina10112005@gmail.com>
2026-03-23 16:49:41 +0200

    Добавлена логика модального окна (ShowFullItem)

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={"./img/" + this.props.item.img} alt={this.props.item.title}  />
+        <img src={"./img/" + this.props.item.img} alt={this.props.item.title} onClick={() => this.props.onShowItem(this.props.item)} />
         <h2>{this.props.item.title}</h2>
         <p>{this.props.item.desc}</p>
         <b>{this.props.item.price}$</b>
--- src/components/ShowFullItem.js
+++ src/components/ShowFullItem.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react'
 export class ShowFullItem extends Component {
   render() {
     return (
-      <div>
-        
+      <div className='full-item' >
+        <div className="">
+          <img src={"./img/" + this.props.item.img} alt={this.props.item.title} onClick={() => this.props.onShowItem(this.props.item)} />
+        <h2>{this.props.item.title}</h2>
+        <p>{this.props.item.desc}</p>
+        <b>{this.props.item.price}$</b>
+        <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+        </div>
       </div>
     )
   }
